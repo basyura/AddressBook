7cd88e94afb5927a364152499b0846c48de400ea
basyura <basyura@gmail.com>
2019-04-14 23:16:48 +0900

    add delete menu

---
+++ AddressBook/Actions/AddressBook/Delete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eleve;
+using AddressBook.Models;
+
+namespace AddressBook.Actions.AddressBook
+{
+    public class Delete : AddressBookActionBase
+    {
+        public override Task<ActionResult> Execute(object sender, EventArgs args, object obj)
+        {
+
+            Address item = ViewModel.SelectedItem;
+
+            ViewModel.Sources.Remove(item);
+            ViewModel.Addresses.Remove(item);
+
+            return SuccessTask;
+        }
+    }
+}
